48f6af50e080569a87edb27170e3d2de88956d61
NeethigaSivakumar <neethiga2001@gmail.com>
2025-06-03 14:10:33 +0530

    String problems

--- src/src/main/java/com/example/String/IndexOfFirstInOccurrenceInString.java
+++ src/src/main/java/com/example/String/IndexOfFirstInOccurrenceInString.java
@@ -1,20 +1,35 @@
 package com.example.String;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class IndexOfFirstInOccurrenceInString {
 
     //Time O(n*m) Space O(m)
     public int strStr(String haystack, String needle) {
-        if(needle.length()>haystack.length()){
+        if (needle.length() > haystack.length()) {
             return -1;
-        }else if(needle.length()==haystack.length() && needle.equals(haystack)){
+        } else if (needle.length() == haystack.length() && needle.equals(haystack)) {
             return 0;
         }
-        for(int i=0;i<=haystack.length()-needle.length();i++){
-            String cur=haystack.substring(i,i+needle.length());
-            if(cur.equals(needle)){
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            String cur = haystack.substring(i, i + needle.length());
+            if (cur.equals(needle)) {
                 return i;
             }
         }
         return -1;
     }
+
+    //Time O(n*m) Space O(n*m)
+    public int strStr1(String haystack, String needle) {
+        Map<String,Integer> map = new HashMap<>();
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            map.put(haystack.substring(i, i + needle.length()),i);
+        }
+        return map.containsKey(needle)?map.get(needle):-1;
+    }
+
 }
