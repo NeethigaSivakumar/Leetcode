f98a9648fe40a0b1e25d18e8c0826cb9917e6e9e
NeethigaSivakumar <neethiga2001@gmail.com>
2025-06-16 10:50:48 +0530

    Tree

---
+++ src/src/main/java/com/example/Trees/LCAOfBinarySearchTree.java
@@ -0,0 +1,18 @@
+package com.example.Trees;
+
+public class LCAOfBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val>root.val && q.val>root.val) {
+                root=root.right;
+            }
+            else if (p.val<root.val && q.val<root.val) {
+                root=root.left;
+            }
+            else{
+                return root;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/src/main/java/com/example/Trees/LCAOfBinaryTree.java
@@ -0,0 +1,18 @@
+package com.example.Trees;
+
+public class LCAOfBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root != null) {
+            if (root.val == p.val || root.val == q.val) {
+                return root;
+            }
+            TreeNode l = lowestCommonAncestor(root.left, p, q);
+            TreeNode r = lowestCommonAncestor(root.right, p, q);
+            if (l != null && r != null) {
+                return root;
+            }
+            return l!=null?l:r;
+        }
+        return null;
+    }
+}
